1c670089433700fe4c5e016ec1058284d95f50fe
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-02 10:37:42 -0300

    retrieving data from server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
-export default function Home() {
+import { GetStaticProps } from 'next'
+import { api } from '../services/api';
+
+interface IEpisodes {
+  id: string;
+  title: string;
+  member: string;
+}
+
+interface IHomeProps {
+  episodes: IEpisodes[];
+}
+
+export default function Home(props: IHomeProps) {
   return (
     <h1>Index</h1>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+  const episodes = response.data;
+
+  return {
+    props: {
+      episodes,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
