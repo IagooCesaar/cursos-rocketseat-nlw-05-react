aad91464466a98e7e0436ceca7f6fc7374156375
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-02 13:50:45 -0300

    navigation to episode detail's page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
+
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+
 import { api } from '../services/api';
 import { convertDurationToTimeSting } from '../utils/convertDurationToTimeString';
 
@@ -12,7 +15,6 @@ interface IEpisodes {
   title: string;
   members: string;
   thumbnail: string;
-  description: string;
   duration: number;
   durationAsString: string;
   url: string;
@@ -42,7 +44,9 @@ export default function Home({ allEpisodes, latestEpisodes }: IHomeProps) {
                   objectFit="cover"
                 />
                 <div className={styles.episodeDetail} >
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
@@ -60,12 +64,14 @@ export default function Home({ allEpisodes, latestEpisodes }: IHomeProps) {
         <h2>Demais episódios</h2>
         <table cellSpacing={0}>
           <thead>
-            <th></th>
-            <th>Podcast</th>
-            <th>Integrantes</th>
-            <th>Data</th>
-            <th>Duração</th>
-            <th></th>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
             {allEpisodes.map(episode => {
@@ -83,7 +89,9 @@ export default function Home({ allEpisodes, latestEpisodes }: IHomeProps) {
                     />
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+                    <Link href={`episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                   </td>
                   <td>{episode.members}</td>
                   <td style={{
@@ -120,7 +128,6 @@ export const getStaticProps: GetStaticProps = async () => {
       id: episode.id,
       title: episode.title,
       members: episode.members,
-      description: episode.description,
       thumbnail: episode.thumbnail,
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
         locale: ptBR
